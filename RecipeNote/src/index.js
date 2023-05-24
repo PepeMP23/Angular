b37501fdac1282620620afd20696c29c865d4e19
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-05-23 19:31:56 -0600

    Avance Final Mayo

---
+++ RecipeNote/src/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { OpenAIService } from './open-ai.js'
+import { config } from 'dotenv';
+
+config({ path: '.env'});
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+// Obtener todas las recetas
+app.get('/recipes', (req, res) => {
+    // Todo la funcionalidad
+    res.send('Hello GET!');
+});
+
+// Para crear una receta
+app.post('/recipes', async (req, res) => {
+    if(!req.body || !req.body.ingredients){
+        res.status(400).json({ error: true, message: 'No hay ingredientes.' });
+        return;
+    }
+
+    const { ingredients } = req.body;
+    const openAI = new OpenAIService();
+    const recipe = await openAI.getRecipe(ingredients);
+    res.json({...recipe});
+    
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+    console.log(`Todo bien`);
+})
---
+++ RecipeNote/src/open-ai.js
@@ -0,0 +1,78 @@
+import {Configuration, OpenAIApi} from 'openai';
+import * as axios from 'axios';
+
+export class OpenAIService {
+    openai;
+    constructor() {
+        const configuration = 
+        new Configuration({
+            apiKey: process.env['OPENAI_KEY'],
+            organization: process.env['OPENAI_ORG']
+        });
+        this.openai  = new OpenAIApi(configuration);
+    }
+
+    async generateRecipe(prompt) {
+        const response = await this.openai.createChatCompletion({
+            model: 'gpt-3.5-turbo-0301',
+            messages: [
+                { role: 'user', name: 'App', content: `
+                    Instrucciones: Eres una inteligencia artificial que genera recetas basado en los
+                    ingredientes que te proporcionen.
+                    - Siempre vas a contestar con una lista de instrucciones en JSON. Sin ningun 
+                    relleno, sin saludos, todo directo.
+                    - Si no te pasan una lista de ingredientes, vas a contestar con { "error":true}.
+                    - Tus contestaciones van a ser las siguientes:
+                    {
+                        "title": "$nombre_receta",
+                        "ingredients": ["$ingrediente1", "$ingrediente2", ...],
+                        "instructions": ["instruccion1", "$instruccion2", ...],
+                        "category": "$categoria", // Americana, Mexicana, Italiana, etc.
+                        "minutes": $minutos, // Tiempo de preparación
+                    }
+                    - Si no entiendes lo que te pasen, o no son ingredientes vas a contestar con
+                      { "error":true}
+                    - No es necesario utilizar todos los ingredientes.
+                    - Siempre incluye los condimentos básicos, como sal, pimienta, azúcar, además 
+                      de agua.
+                `},
+                { role : "user", name: 'User', content : prompt}
+            ],
+            max_tokens: 1000
+        });
+        return response;
+    }
+
+    /**
+     * 
+     * @param {string} ingredients 
+     */
+
+    async getRecipe(ingredients) {
+        const response = await this.generateRecipe(ingredients);
+        const json = this.getJSONFromResponse(response.data.choices[0].message.content);
+        if (Object.keys(json).includes('error')) {
+            return json;
+        }
+        const recipe = json;
+        const generatedRecipe = {
+            title: recipe.title,
+            category: recipe.category,
+            minutes: recipe.ingredients,
+            instructions: recipe.instructions,
+            prompt: ingredients,
+            stars: 0,
+            imageUrl: '',
+        };
+        return generatedRecipe;
+    }
+
+    getJSONFromResponse(contentResponse) {
+        try {
+            const json = JSON.parse(contentResponse);
+            return json;
+        } catch (error) {
+            return { error: true };
+        }
+    }
+}
